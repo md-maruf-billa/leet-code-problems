e24b82d1b84a10320c123fcd209d5a12ed097285
md-maruf-billa <eng.marufbilla@gmail.com>
2025-06-07 07:49:36 +0600

    solved roman to int

---
+++ easy/romanToInt.js
@@ -0,0 +1,26 @@
+const romanToInt = (s) => {
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+    let total = 0;
+    for (let i = 0; i < s.length; i++) {
+        const current = romanMap[s[i]];
+        const next = romanMap[s[i + 1]];
+
+        if (next > current) {
+            total += (next - current);
+            i++;
+        } else {
+            total += current;
+        }
+    }
+    return total;
+}
+
+console.log(romanToInt("LVIII"))
